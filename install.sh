bf9a7d0acd2d8791f88df5c37bd67403a13307d9
Pawel Palucki <pawel.palucki@intel.com>
2014-11-10 17:05:54 +0100

    tmux copy-mode tunning

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sv ~/dotfiles/.screenrc ~/.screenrc
 ln -sv ~/dotfiles/.vim ~/.vim
 ln -sv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sv ~/dotfiles/.ackrc ~/.ackrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo done
