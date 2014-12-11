ce098259d47321fd6ac49ccacbf02922183834dc
Pawel Palucki <pawel.palucki@intel.com>
2014-12-11 21:16:04 +0100

    better installer

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ln -sv ~/dotfiles/.vimrc ~/.vimrc
 ln -sv ~/dotfiles/.screenrc ~/.screenrc
-ln -sv ~/dotfiles/.vim ~/.vim
+ln -sv ~/dotfiles/.vim ~/
 ln -sv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sv ~/dotfiles/.ackrc ~/.ackrc
 ln -sv ~/dotfiles/.ctags ~/.ctags
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
 echo done
