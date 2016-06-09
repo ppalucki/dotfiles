1a089cab9df6c01ea72ac69499efcb1e26aafcea
Pawel Palucki <pawel.palucki@intel.com>
2016-06-09 14:58:07 +0200

    WIP

--- proxy/install.sh
+++ proxy/install.sh
@@ -1,6 +1,9 @@
-ln -s /home/ppalucki/dotfiles/proxy/redsocks.service /etc/systemd/system/
-ln -s /home/ppalucki/dotfiles/proxy/redsocksFW.service /etc/systemd/system/
-systemctl daemon-firewall
+# ln -s /home/ppalucki/dotfiles/proxy/redsocks.service /etc/systemd/system/
+# ln -s /home/ppalucki/dotfiles/proxy/redsocksFW.service /etc/systemd/system/
+cp /home/ppalucki/dotfiles/proxy/redsocks.service /etc/systemd/system/
+cp /home/ppalucki/dotfiles/proxy/redsocksFW.service /etc/systemd/system/
+ls /etc/systemd/system/red*
+systemctl daemon-reload
 systemctl enable redsocks redsocksFW
 systemctl start redsocks redsocksFW
-systemctl status redsocks redsocksFW
+systemctl status redsocks redsocksFW | cat
