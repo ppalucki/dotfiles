23229cdbf3c90f092be7e62620873421673ab547
Pawel Palucki <pawel.palucki@intel.com>
2016-07-13 14:01:40 +0200

    better proxy

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -4,7 +4,7 @@
 # echo "net.ipv4.conf.all.route_localnet=1" >>/etc/sysctl.conf
 # # sysctl -w net.ipv4.conf.all.route_localnet=1
 # # sysctl -p
-# sysctl -n net.ipv4.conf.all.route_localnet=1
+sysctl -n net.ipv4.conf.all.route_localnet=1
 
 ### -------------------  NAT ------------------------------------
 #Creating Chain for transparency rules and adding for flow chains (output and "routing")
@@ -18,15 +18,25 @@ iptables -I PREROUTING -t nat -j REDSOCKS # required by eg. docker
 #     iptables -A REDSOCKS -t nat -d $subnet -j RETURN
 # done
 # Return if LAN forward to proxy for http (on proxy-mu ip)/https
-iptables -A REDSOCKS -t nat -d 127.0.0.1 -j RETURN
+iptables -A REDSOCKS -t nat -d 127.0.0.1/32 -j RETURN
 iptables -A REDSOCKS -t nat -d 10.0.0.0/8 -j RETURN
 iptables -A REDSOCKS -t nat -d 172.17.0.0/16 -j RETURN # coreos-kubernetes
 iptables -A REDSOCKS -t nat -d 10.4.0.0/16 -j RETURN # tectonic
 iptables -A REDSOCKS -t nat -d 10.217.247.236/32 -j RETURN # proxy-mu
 iptables -A REDSOCKS -t nat -d 163.33.7.140/32 -j RETURN # proxy-chain
-# only https/http opmtyalizcja ruch !!!
+iptables -A REDSOCKS -t nat -d 127.0.0.1 -j RETURN # some services (like rabbitmq) need this:
+
+### ESSENTIAL CODE HERE!!!
+# everything trhough redsocks !!!
+#iptables -A REDSOCKS -t nat -p tcp -m tcp -j DNAT --to-destination 127.0.0.1:911
+
+# http without redsocks (proxy-mu.intel.com)
 iptables -A REDSOCKS -t nat -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.217.247.236:911
-iptables -A REDSOCKS -t nat -p tcp -m tcp --dport 443 -j DNAT --to-destination 127.0.0.1:1080
+# https -> redsocks
+iptables -A REDSOCKS -t nat -p tcp -m tcp --dport 443 -j DNAT --to-destination 127.0.0.1:911
+# ssh -> redsocks
+iptables -A REDSOCKS -t nat -p tcp -m tcp --dport 22 -j DNAT --to-destination 127.0.0.1:911
+
 ## redirect everything else
 # iptables -A REDSOCKS -t nat -p tcp -m tcp -j DNAT --to-destination 127.0.0.1:1080
 ## sourcegraph binnary gada po rpc do serweera
@@ -34,7 +44,7 @@ iptables -A REDSOCKS -t nat -p tcp -m tcp --dport 443 -j DNAT --to-destination 1
 
 ### ------------------- FILTER --------------------------------
 #adding filter rules for transock_ev, dns, http_proxy and packets that are allready accepted for "routing"
-iptables -t filter -I INPUT -p tcp -m tcp --dport 1080 -j ACCEPT
+iptables -t filter -I INPUT -p tcp -m tcp --dport 911 -j ACCEPT
 iptables -t filter -I FORWARD -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t filter -I FORWARD -p tcp -m tcp --dport 911 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t filter -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 
