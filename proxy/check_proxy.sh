b368ab1d09562904f4098ad7809b5a5e9c9dc8d4
Pawel Palucki <pawel.palucki@intel.com>
2016-08-08 12:28:26 +0200

    better proxy check scipt

--- proxy/check_proxy.sh
+++ proxy/check_proxy.sh
@@ -1,7 +1,13 @@
 set -e
-set -x
+# set -x
+echo "================================ check process "
 pgrep redsocks
+echo "================================ check service "
 systemctl status redsocks
+echo "================================ check https "
 wget https://www.google.com -O /dev/null
+echo "================================ check http "
 wget http://www.google.com -O /dev/null
+echo "================================ check ssh "
 ssh -T git@github.com
+echo "OK"
