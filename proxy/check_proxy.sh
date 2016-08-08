32cc636fa26b87aed570e7bd7c9fdc24b77b5fe7
Pawel Palucki <pawel.palucki@intel.com>
2016-08-08 18:15:26 +0200

    better proxy

--- proxy/check_proxy.sh
+++ proxy/check_proxy.sh
@@ -4,10 +4,10 @@ echo "================================ check process "
 pgrep redsocks
 echo "================================ check service "
 systemctl status redsocks
-echo "================================ check https "
-wget https://www.google.com -O /dev/null
 echo "================================ check http "
-wget http://www.google.com -O /dev/null
+wget --timeout=5 http://www.google.com -O /dev/null
+echo "================================ check https "
+wget --timeout=5 https://www.google.com -O /dev/null
 echo "================================ check ssh "
-ssh -T git@github.com
+ssh -o ConnectTimeout=5s -T git@github.com
 echo "OK"
