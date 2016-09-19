92c227d68fe705d6ba131ff2be43463b8d8ee39d
Pawel Palucki <pawel.palucki@intel.com>
2016-09-19 10:07:46 +0200

    more and more about go and vim

--- compose/docker-intellij/start.sh
+++ compose/docker-intellij/start.sh
@@ -1,5 +1,8 @@
 # pre configuration
 
+############ docker auth
+# chmod o+rw /var/run/docker.sock
+
 ############ xauth
 #https://debian-administration.org/article/494/Getting_X11_forwarding_through_ssh_working_after_running_su
 # xauth list
@@ -11,18 +14,18 @@
 # fs.inotify.max_user_watches = 524288
 # sudo sysctl -p --system
 
+PREF_DIR=${HOME}/.IdeaIC_docker
+IDEAVIM_RC=${HOME}~/.ideavimrc
 
 
-
-PREF_DIR=${HOME}/.IdeaIC15_docker
-
-sudo -E docker run -ti \
+docker run -d \
+           --name docker-intellji \
            -e DISPLAY=${DISPLAY} \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v ${PREF_DIR}:/home/developer/.IdeaIC15 \
+           -v ${PREF_DIR}:/home/developer/.IdeaIC2016.2 \
+           -v ${IDEAVIM_RC}:/home/developer/.ideavimrc \
            -v ${GOPATH}:/home/developer/go \
            -v ~/.Xauthority:/home/developer/.Xauthority \
            --net host \
            dlsniper/docker-intellij
-
            #--privileged \
