8cb80f2e24cc37e1d2e9ad6f20edf5ced080e7c7
Pawel Palucki <pawel.palucki@sap.com>
2024-03-18 15:09:49 +0100

    fix oh zsh hihglight + ctags

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,1 +1,3 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
