aa0dc2a100a07851881ed638fba13625fd708c6c
Pawel Palucki <pawel.palucki@sap.com>
2024-03-26 16:44:06 +0100

    fix proxy and krew

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -30,6 +30,7 @@ iptables -A REDSOCKS -t nat -d 172.18.0.0/16 -j RETURN # coreos-kubernetes
 iptables -A REDSOCKS -t nat -d 172.18.0.0/16 -j RETURN # coreos-kubernetes
 iptables -A REDSOCKS -t nat -d 172.19.0.0/16 -j RETURN # coreos-kubernetes
 iptables -A REDSOCKS -t nat -d 172.20.0.0/16 -j RETURN # coreos-kubernetes
+iptables -A REDSOCKS -t nat -d 172.23.0.0/16 -j RETURN # coreos-kubernetes
 iptables -A REDSOCKS -t nat -d 10.4.0.0/16 -j RETURN # tectonic
 iptables -A REDSOCKS -t nat -d 10.217.247.236/32 -j RETURN # proxy-mu
 iptables -A REDSOCKS -t nat -d 163.33.0.0/16 -j RETURN # proxy-chain and teamforge
