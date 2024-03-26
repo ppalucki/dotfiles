dbbfeafc7809845ebaaff31d014ff858051e943d
Pawel Palucki <pawel.palucki@sap.com>
2024-03-26 16:45:30 +0100

    fugitve + next change + new k9s

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
+echo PLEASE USE install_2022.sh # this is not longer suported
 #!/bin/bash
-ln -sv ~/dotfiles/.vimrc ~/.vimrc
-# ln -sv ~/dotfiles/.screenrc ~/.screenrc
-ln -sv ~/dotfiles/.vim ~/
-ln -sv ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sv ~/dotfiles/.ackrc ~/.ackrc
-ln -sv ~/dotfiles/.ctags ~/.ctags
-# ln -sv ~/dotfiles/.bash_aliases ~/.bash_aliases
-# ln -sv ~/dotfiles/.ideavimrc ~/.ideavimrc 
-# ln -sv ~/dotfiles/.gdb.py ~/.gdb.py
-# ln -sv ~/dotfiles/.gdbinit ~/.gdbinit
-ln -sv ~/dotfiles/.zshrc ~/.zshrc
-# ln -sv ~/dotfiles/.pdbrc.py ~/.pdbrc.py
-mkdir -p ~/.aws/cli
-ln -sv ~/dotfiles/aws/alias ~/.aws/cli/alias
-echo done
+#####ln -sv ~/dotfiles/.vimrc ~/.vimrc
+###### ln -sv ~/dotfiles/.screenrc ~/.screenrc
+#####ln -sv ~/dotfiles/.vim ~/
+#####ln -sv ~/dotfiles/.gitconfig ~/.gitconfig
+#####ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
+#####ln -sv ~/dotfiles/.ackrc ~/.ackrc
+#####ln -sv ~/dotfiles/.ctags ~/.ctags
+###### ln -sv ~/dotfiles/.bash_aliases ~/.bash_aliases
+###### ln -sv ~/dotfiles/.ideavimrc ~/.ideavimrc 
+###### ln -sv ~/dotfiles/.gdb.py ~/.gdb.py
+###### ln -sv ~/dotfiles/.gdbinit ~/.gdbinit
+#####ln -sv ~/dotfiles/.zshrc ~/.zshrc
+###### ln -sv ~/dotfiles/.pdbrc.py ~/.pdbrc.py
+#####mkdir -p ~/.aws/cli
+#####ln -sv ~/dotfiles/aws/alias ~/.aws/cli/alias
+#####echo done
--- install_2022.sh
+++ install_2022.sh
@@ -3,3 +3,4 @@ ln -sv $PWD/vimrc_2022 ~/.vimrc
 ln -sv $PWD/tmux.conf_2022 ~/.tmux.conf
 ln -sv $PWD/gitconfig_2022 ~/.gitconfig
 ln -sv $PWD/zshrc_2022 ~/.zshrc
+ln -sv $PWD/k9s ~/.config/k9s
