1e2ae42ec364ea855dd762bb29166f3753286536
Pawel Palucki <pawel.palucki@sap.com>
2024-11-07 12:38:16 +0100

    yaml vim tagbar with universal tags + git aliases for ignoring

--- install_2022.sh
+++ install_2022.sh
@@ -4,3 +4,8 @@ ln -sv $PWD/tmux.conf_2022 ~/.tmux.conf
 ln -sv $PWD/gitconfig_2022 ~/.gitconfig
 ln -sv $PWD/zshrc_2022 ~/.zshrc
 ln -sv $PWD/k9s ~/.config/
+#ln -sv $PWD/.ctags ~/.ctags
+mkdir -p ~/.ctags.d/
+ln -sv $PWD/.ctags ~/.ctags.d/yaml.ctags
+
+
