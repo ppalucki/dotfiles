4f627d3111a46f29b17c65f295f19e2a247b4006
Pawel Palucki <pawel.palucki@sap.com>
2024-11-28 04:45:54 -0800

    add folder

--- install_2022.sh
+++ install_2022.sh
@@ -3,9 +3,10 @@ ln -sv $PWD/vimrc_2022 ~/.vimrc
 ln -sv $PWD/tmux.conf_2022 ~/.tmux.conf
 ln -sv $PWD/gitconfig_2022 ~/.gitconfig
 ln -sv $PWD/zshrc_2022 ~/.zshrc
+mkdir -p ~/.config/
 ln -sv $PWD/k9s ~/.config/
 #ln -sv $PWD/.ctags ~/.ctags
 mkdir -p ~/.ctags.d/
 ln -sv $PWD/.ctags ~/.ctags.d/yaml.ctags
 
-
+echo "Please also execute: ./install-oh-my-zsh.sh"
